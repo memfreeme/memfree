8d8f2a52e1e241c6ebe7d67f9e417def288ffd74
ahaapple <work@ahaapple.com>
2024-07-25 20:26:31 +0800

    Update pricing page

--- README.md
+++ README.md
@@ -44,8 +44,8 @@ MemFree is a hybrid AI search engine that simultaneously performs searches on yo
 
 ## Update
 
-- **2024-07-25**: Supports Rerank to get better answer when hybrid search
-- **2024-07-24**: Supports context-based continuous search and questioning
+- **2024-07-25**: Support Rerank to get better answer when hybrid search
+- **2024-07-24**: Support context-based continuous search and questioning
 - **2024-07-23**: Support claude-3-5-sonnet AI Model
 - **2024-07-21**: Support one command deploy Searxng and Vector Service with fly.io
 - **2024-07-19**: Support GPT-4o-mini AI Model
--- frontend/config/subscriptions.ts
+++ frontend/config/subscriptions.ts
@@ -5,10 +5,10 @@ export const pricingData: SubscriptionPlan[] = [
         title: 'Free',
         description: 'For Beginners',
         benefits: [
-            'Up to 10 searchs per day',
+            'Up to 10 Searches per day',
             'Up to 10 bookmarks and web pages to index',
-            'Support GPT-3.5 AI Model',
-            'Support web and chrome extension',
+            'Support GPT-4o-mini AI Model',
+            'Support Web and Chrome Extension',
         ],
         limitations: [],
         prices: {
@@ -24,9 +24,11 @@ export const pricingData: SubscriptionPlan[] = [
         title: 'Pro',
         description: 'Unlock Advanced Features',
         benefits: [
-            'Up to 100 searchs per day',
-            'Up to 1000 bookmarks and web pages to index',
-            'Support GPT-3.5 and GPT-4o AI Model',
+            'Unlimited Basic AI Searches Per day',
+            'Up to 1000 Bookmarks and Web Pages to Index',
+            'Support GPT-4o & Claude-3.5-sonnet AI Model',
+            'Support Context-based Continuous Search',
+            'Support Rerank to get Better search results',
         ],
         limitations: [],
         prices: {
@@ -42,8 +44,9 @@ export const pricingData: SubscriptionPlan[] = [
         title: 'Premium',
         description: 'For Power Users',
         benefits: [
-            'Unlimited searchs per day',
-            'Unlimited bookmarks and web pages to index',
+            'Unlimited Basic AI Searches Per day',
+            'Unlimited Expert AI Searches Per day',
+            'Unlimited Bookmarks and Web Pages to Index',
             'Priority customer support',
         ],
         limitations: [],
