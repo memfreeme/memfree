8a9aa16ea5ced47087ae39bba7dd74595c605173
ahaapple <151364778+ahaapple@users.noreply.github.com>
2024-08-15 16:01:30 +0800

    Update layout.tsx

--- frontend/app/(marketing)/layout.tsx
+++ frontend/app/(marketing)/layout.tsx
@@ -1,15 +1,13 @@
 import { getCurrentUser } from '@/lib/session';
 import { Suspense } from 'react';
-<<<<<<< HEAD
 import SiteHeader from '@/components/layout/site-header';
 import { SimpleSiteFooter } from '@/components/layout/simple-site-footer';
 import Featurebase from '@/components/featurebase';
 import { mainNavConfig, siteConfig } from '@/config';
-=======
+
 import MobileHeader from '@/components/layout/mobile-header';
 import AdminPanelLayout from '@/components/layout/admin';
 import { Sidebar } from '@/components/sidebar/sidebar';
->>>>>>> 0a2bca1 (sidebar)
 
 interface MarketingLayoutProps {
     children: React.ReactNode;
@@ -24,7 +22,6 @@ export default async function MarketingLayout({
 }: MarketingLayoutProps) {
     const user = await getCurrentUser();
     return (
-<<<<<<< HEAD
         <div className="flex min-h-screen flex-col">
             <Featurebase user={user}></Featurebase>
             <Suspense fallback="...">
@@ -33,7 +30,6 @@ export default async function MarketingLayout({
             <main className="flex-1">{children}</main>
             <SimpleSiteFooter />
         </div>
-=======
         // <div className="flex flex-col flex-1 min-h-screen">
         //     <MobileHeader user={user} />
         //     <main className="relative flex h-lvh overflow-hidden">
@@ -43,9 +39,8 @@ export default async function MarketingLayout({
         //         {children}
         //     </main>
         // </div>
-        <AdminPanelLayout aside={<SidebarDesktop />}>
-            {children}
-        </AdminPanelLayout>
->>>>>>> 0a2bca1 (sidebar)
+        // <AdminPanelLayout aside={<SidebarDesktop />}>
+        //     {children}
+        // </AdminPanelLayout>
     );
 }
