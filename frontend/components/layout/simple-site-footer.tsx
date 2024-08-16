2381b3ae1d22431d7140c7d848bea29400eedd1e
ahaapple <work@ahaapple.com>
2024-08-16 14:09:57 +0800

    Update site footer

--- frontend/components/layout/simple-site-footer.tsx
+++ frontend/components/layout/simple-site-footer.tsx
@@ -7,16 +7,27 @@ export function SimpleSiteFooter() {
         <footer>
             <div className="mx-auto p-4 pt-10">
                 <ul className="mt-8 flex justify-center gap-6 md:gap-8">
-                    <li className="mx-4">
+                    <li className="mx-2">
                         <Link
                             href={siteConfig.links.github}
                             target="_blank"
+                            rel="noopener noreferrer"
                             aria-label="MemFree GitHub"
                         >
                             <Icons.gitHub className="size-5 hover:text-primary" />
                         </Link>
                     </li>
-                    <li className="mx-4">
+                    <li className="mx-2">
+                        <Link
+                            href={siteConfig.links.twitter}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            aria-label="MemFree Twitter"
+                        >
+                            <Icons.twitter className="size-5 hover:text-primary" />
+                        </Link>
+                    </li>
+                    <li className="mx-2">
                         <Link
                             data-featurebase-link
                             href="https://feedback.memfree.me"
@@ -27,26 +38,17 @@ export function SimpleSiteFooter() {
                             <Icons.heart className="size-5 hover:text-primary" />
                         </Link>
                     </li>
-                    <li className="mx-4">
+                    <li className="mx-2">
                         <Link
                             href="https://chromewebstore.google.com/detail/memfree/dndjodcanbhkomcgihbhcejogiimdmpk"
                             target="_blank"
+                            rel="noopener noreferrer"
                             aria-label="MemFree Browser Extension"
                         >
                             <Icons.chrome className="size-5 hover:text-primary" />
                         </Link>
                     </li>
-                    {/* <li className="mx-4">
-                        <Link
-                            href={siteConfig.links.twitter}
-                            target="_blank"
-                            rel="noreferrer"
-                            aria-label="MemFree Twitter"
-                        >
-                            <Icons.twitter className="size-5 hover:text-primary" />
-                        </Link>
-                    </li>
-                    <li className="mx-4">
+                    <li className="mx-2">
                         <a
                             href={siteConfig.links.discord}
                             target="_blank"
@@ -55,7 +57,7 @@ export function SimpleSiteFooter() {
                         >
                             <Icons.discord className="size-5 hover:text-primary" />
                         </a>
-                    </li> */}
+                    </li>
                 </ul>
             </div>
         </footer>
