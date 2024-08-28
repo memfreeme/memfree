56477306a8142a3aaef96252108e738d2cfbd239
ahaapple <work@ahaapple.com>
2024-08-28 19:51:17 +0800

    Improve search-bar UI

--- frontend/components/search-bar.tsx
+++ frontend/components/search-bar.tsx
@@ -87,111 +87,114 @@ const SearchBar: React.FC<Props> = ({ handleSearch }) => {
 
     return (
         <div className="w-full text-center">
-            <div className="flex items-center relative mx-auto w-full">
-                <div className="flex flex-col w-full">
-                    {file && (
-                        <Image
-                            src={imageUrl}
-                            key={file.name}
-                            alt={file.name}
-                            width={100}
-                            height={100}
-                            loading="lazy"
-                            className="aspect-square shrink-0 rounded-lg object-cover m-4"
-                        />
-                    )}
-                    <TextareaAutosize
-                        value={content}
-                        minRows={3}
-                        maxRows={10}
-                        aria-label="Search"
-                        className="w-full border-input bg-transparent px-4 py-2 text-sm shadow-sm placeholder:text-muted-foreground focus:border-primary overflow-y-auto  outline-0 ring-0  focus-visible:outline-none focus-visible:ring-0 resize-none border-2 rounded-xl"
-                        onKeyDown={handleInputKeydown}
-                        onChange={(e) => setContent(e.target.value)}
+            <div className="flex flex-col relative mx-auto w-full border-2 rounded-lg focus-within:border-primary">
+                {file && (
+                    <Image
+                        src={imageUrl}
+                        key={file.name}
+                        alt={file.name}
+                        width={100}
+                        height={100}
+                        loading="lazy"
+                        className="aspect-square shrink-0 rounded-lg object-cover m-4"
                     />
-                </div>
-
-                <div className="absolute left-0 bottom-0 mb-1 ml-2 flex items-center space-x-4">
-                    <Tooltip>
-                        <TooltipTrigger asChild>
-                            <button
-                                type="button"
-                                aria-label="Index"
-                                className="text-gray-500 hover:text-primary flex items-center"
-                                onClick={() => {
-                                    if (!user) {
-                                        signInModal.onOpen();
-                                    } else {
-                                        uploadModal.onOpen();
-                                    }
-                                }}
-                            >
-                                <span className="sr-only">Index</span>
-                                <Icons.mylink size={24} strokeWidth={2} />
-                            </button>
-                        </TooltipTrigger>
-                        <TooltipContent>
-                            Enhance AI Search by Indexing the Web Pages and
-                            Local Files
-                        </TooltipContent>
-                    </Tooltip>
-
-                    <Tooltip>
-                        <TooltipTrigger asChild>
-                            <div>
-                                <input
-                                    {...getInputProps()}
-                                    ref={dropzoneRef}
-                                    className="hidden"
-                                />
+                )}
+                <TextareaAutosize
+                    value={content}
+                    minRows={1}
+                    maxRows={10}
+                    aria-label="Search"
+                    className="w-full border-0 bg-transparent p-4 mb-8 text-sm placeholder:text-muted-foreground overflow-y-auto  outline-0 ring-0  focus-visible:outline-none focus-visible:ring-0 resize-none"
+                    onKeyDown={handleInputKeydown}
+                    onChange={(e) => setContent(e.target.value)}
+                ></TextareaAutosize>
+                <div className="flex relative">
+                    <div className="absolute left-0 bottom-0 mb-1 ml-2 mt-6 flex items-center space-x-4">
+                        <Tooltip>
+                            <TooltipTrigger asChild>
                                 <button
                                     type="button"
-                                    disabled={isUploading}
-                                    aria-label="Image upload"
+                                    aria-label="Index"
                                     className="text-gray-500 hover:text-primary flex items-center"
                                     onClick={() => {
                                         if (!user) {
                                             signInModal.onOpen();
                                         } else {
-                                            openFileDialog();
+                                            uploadModal.onOpen();
                                         }
                                     }}
                                 >
-                                    <span className="sr-only">
-                                        Image upload
-                                    </span>
-                                    {isUploading ? (
-                                        <Icons.spinner
-                                            size={24}
-                                            strokeWidth={2}
-                                            className="animate-spin"
-                                        />
-                                    ) : (
-                                        <ImageIcon size={24} strokeWidth={2} />
-                                    )}
+                                    <span className="sr-only">Index</span>
+                                    <Icons.mylink size={24} strokeWidth={2} />
                                 </button>
-                            </div>
-                        </TooltipTrigger>
-                        <TooltipContent>Attach Image</TooltipContent>
-                    </Tooltip>
-                </div>
+                            </TooltipTrigger>
+                            <TooltipContent>
+                                Enhance AI Search by Indexing the Web Pages and
+                                Local Files
+                            </TooltipContent>
+                        </Tooltip>
 
-                <div className="absolute right-0 bottom-0 mb-1 mr-2">
-                    <Tooltip>
-                        <TooltipTrigger asChild>
-                            <button
-                                type="button"
-                                aria-label="Search"
-                                disabled={content.trim() === '' || isUploading}
-                                className="text-gray-500 hover:text-primary disabled:cursor-not-allowed disabled:opacity-50"
-                                onClick={handleClick}
-                            >
-                                <span className="sr-only">Search</span>
-                                <SendHorizontal size={24} strokeWidth={2} />
-                            </button>
-                        </TooltipTrigger>
-                        <TooltipContent>Send (Enter)</TooltipContent>
-                    </Tooltip>
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <div>
+                                    <input
+                                        {...getInputProps()}
+                                        ref={dropzoneRef}
+                                        className="hidden"
+                                    />
+                                    <button
+                                        type="button"
+                                        disabled={isUploading}
+                                        aria-label="Image upload"
+                                        className="text-gray-500 hover:text-primary flex items-center"
+                                        onClick={() => {
+                                            if (!user) {
+                                                signInModal.onOpen();
+                                            } else {
+                                                openFileDialog();
+                                            }
+                                        }}
+                                    >
+                                        <span className="sr-only">
+                                            Image upload
+                                        </span>
+                                        {isUploading ? (
+                                            <Icons.spinner
+                                                size={24}
+                                                strokeWidth={2}
+                                                className="animate-spin"
+                                            />
+                                        ) : (
+                                            <ImageIcon
+                                                size={24}
+                                                strokeWidth={2}
+                                            />
+                                        )}
+                                    </button>
+                                </div>
+                            </TooltipTrigger>
+                            <TooltipContent>Attach Image</TooltipContent>
+                        </Tooltip>
+                    </div>
+                    <div className="absolute right-0 bottom-0 mb-1 mr-2 mt-6">
+                        <Tooltip>
+                            <TooltipTrigger asChild>
+                                <button
+                                    type="button"
+                                    aria-label="Search"
+                                    disabled={
+                                        content.trim() === '' || isUploading
+                                    }
+                                    className="text-gray-500 hover:text-primary disabled:cursor-not-allowed disabled:opacity-50"
+                                    onClick={handleClick}
+                                >
+                                    <span className="sr-only">Search</span>
+                                    <SendHorizontal size={24} strokeWidth={2} />
+                                </button>
+                            </TooltipTrigger>
+                            <TooltipContent>Send (Enter)</TooltipContent>
+                        </Tooltip>
+                    </div>
                 </div>
             </div>
 
