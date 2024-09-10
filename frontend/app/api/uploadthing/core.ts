6aabcb91bea2adfb1a3e744cb2dbf17439afddc8
ahaapple <work@ahaapple.com>
2024-09-10 23:11:04 +0800

    Enlarge the image maxFileSize limit

--- frontend/app/api/uploadthing/core.ts
+++ frontend/app/api/uploadthing/core.ts
@@ -5,7 +5,7 @@ import { UploadThingError } from 'uploadthing/server';
 
 const f = createUploadthing();
 export const ourFileRouter = {
-    imageUploader: f({ image: { maxFileSize: '4MB', maxFileCount: 5 } })
+    imageUploader: f({ image: { maxFileSize: '16MB', maxFileCount: 5 } })
         .middleware(async ({ req }) => {
             const session = await auth();
             if (!session) throw new UploadThingError('Unauthorized');
