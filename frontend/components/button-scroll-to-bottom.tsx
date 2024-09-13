a61e19f5a6fa28757bf3881dbf21d3160a33f9bb
ahaapple <work@ahaapple.com>
2024-09-13 17:19:43 +0800

    Improve scrollToBottom button UI

--- frontend/components/button-scroll-to-bottom.tsx
+++ frontend/components/button-scroll-to-bottom.tsx
@@ -10,19 +10,16 @@ interface ButtonScrollToBottomProps {
     scrollToBottom: () => void;
 }
 
-export function ButtonScrollToBottom({
-    isAtBottom,
-    scrollToBottom,
-}: ButtonScrollToBottomProps) {
+export function ButtonScrollToBottom({ isAtBottom, scrollToBottom }: ButtonScrollToBottomProps) {
     return (
         <button
             className={cn(
-                'absolute top-1/2 right-2 p-2 mr-2 z-10 rounded-lg transition-opacity duration-300 hover:text-primary hover:bg-gray-200 dark:hover:bg-gray-700',
+                'absolute top-1/2 right-2 p-2 mr-2 z-10  bg-purple-500 text-white rounded-full hover:bg-purple-600',
                 isAtBottom ? 'opacity-0' : 'opacity-100',
             )}
             onClick={() => scrollToBottom()}
         >
-            <Icons.arrowDown size={24} strokeWidth={3} />
+            <Icons.arrowDown />
             <span className="sr-only">Scroll to bottom</span>
         </button>
     );
