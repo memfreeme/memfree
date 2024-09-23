2788b1eb4860ef38d8b1dccb055d510f34b188e9
ahaapple <work@ahaapple.com>
2024-09-23 16:36:04 +0800

    Update demo questions

--- frontend/components/search/demo-questions.tsx
+++ frontend/components/search/demo-questions.tsx
@@ -7,10 +7,6 @@ export async function DemoQuestions() {
     const t = await getTranslations('DemoQuestions');
     const demoQuestions = [
         {
-            title: t('title5'),
-            link: '/share/WkKsbm8',
-        },
-        {
             title: t('title1'),
             link: '/share/Ed21pa5',
         },
@@ -18,32 +14,39 @@ export async function DemoQuestions() {
         //     title: t('title2'),
         //     link: '/share/GgxtPak',
         // },
-        // {
-        //     title: t('title3'),
-        //     link: '/share/cTWwJvL',
-        // },
+        {
+            title: t('title3'),
+            link: '/share/E6lIW1t',
+        },
         {
             title: t('title4'),
             link: '/share/WreYuQn',
         },
         {
+            title: t('title5'),
+            link: '/share/WkKsbm8',
+        },
+        {
             title: t('title6'),
             link: '/share/lXNTOIT',
         },
-        // {
-        //     title: t('title7'),
-        //     link: '/share/TZ3wyPV',
-        // },
+        {
+            title: t('title7'),
+            link: '/share/TZ3wyPV',
+        },
     ];
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-8" dir="auto">
-            {demoQuestions.map((example) => (
-                <Link key={example.link} href={example.link} target="_blank">
-                    <div className={`cursor-pointer rounded-lg border bg-white p-4 hover:bg-zinc-50 dark:bg-zinc-950 dark:hover:bg-zinc-900`}>
-                        <div className="text-sm font-semibold">{example.title}</div>
-                    </div>
-                </Link>
-            ))}
+            {demoQuestions
+                .sort(() => Math.random() - 0.5)
+                .slice(0, 4)
+                .map((example) => (
+                    <Link key={example.link} href={example.link} target="_blank">
+                        <div className={`cursor-pointer rounded-lg border bg-white p-4 hover:bg-zinc-50 dark:bg-zinc-950 dark:hover:bg-zinc-900`}>
+                            <div className="text-sm font-semibold">{example.title}</div>
+                        </div>
+                    </Link>
+                ))}
         </div>
     );
 }
