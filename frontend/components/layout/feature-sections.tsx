83bd3e725039c0a4341829795c76bea0669458b4
ahaapple <work@ahaapple.com>
2024-10-16 22:23:41 +0800

    Improve generate ui feature section in dark mode

--- frontend/components/layout/feature-sections.tsx
+++ frontend/components/layout/feature-sections.tsx
@@ -74,8 +74,9 @@ const FeatureSections = () => {
                                 })}
                             >
                                 <h2 className="text-base font-semibold leading-7 text-primary">{feature.header}</h2>
-                                <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{feature.name}</p>
-                                <p className="mt-6 text-lg leading-8 text-gray-600">{feature.description}</p>
+                                <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl">{feature.name}</p>
+                                <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-300">{feature.description}</p>
+
                                 <Link
                                     className={cn(
                                         buttonVariants({
