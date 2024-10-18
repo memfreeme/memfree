761b34e7b8f229c26a82aee3bc1e42fe2c1323de
ahaapple <work@ahaapple.com>
2024-10-18 14:50:47 +0800

    Make EXA_API_KEY optional in local dev

--- frontend/lib/search/exa.ts
+++ frontend/lib/search/exa.ts
@@ -3,26 +3,25 @@ import { SearchOptions, SearchResult, SearchSource } from '@/lib/search/search';
 import { TextSource } from '@/lib/types';
 import { logError } from '@/lib/log';
 
-const exa = new Exa(process.env.EXA_API_KEY);
-
 export class EXASearch implements SearchSource {
     private options: SearchOptions;
+    private exa: Exa;
 
     constructor(params?: SearchOptions) {
         this.options = params || {};
+        this.exa = new Exa(process.env.EXA_API_KEY);
     }
 
     async search(query: string): Promise<SearchResult> {
         try {
             const category = this.options.categories[0];
-            const result = await exa.searchAndContents(query, {
+            const result = await this.exa.searchAndContents(query, {
                 numResults: 10,
                 category: category,
                 type: 'neural',
                 useAutoprompt: true,
             });
-            let texts: TextSource[] = [];
-            texts = result.results.map((c: any) => {
+            const texts: TextSource[] = result.results.map((c: any) => {
                 return {
                     title: c.author,
                     url: c.url,
