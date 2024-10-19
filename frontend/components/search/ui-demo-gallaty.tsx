e7c0f166470209b6e9d8290e849defa79dd4f514
ahaapple <work@ahaapple.com>
2024-10-19 21:26:50 +0800

    Fix github duplicate workflow issue

--- frontend/components/search/ui-demo-gallaty.tsx
+++ frontend/components/search/ui-demo-gallaty.tsx
@@ -7,11 +7,8 @@ const DemoGallery = () => {
     const images = [
         {
             src: 'https://image.memfree.me/memfree-generate-ui-case-pricing-page.png',
-            user: {
-                avatar: 'https://randomuser.me/api/portraits/women/2.jpg',
-                name: 'Jane Doe',
-            },
-            description: 'A Pricing Page for AI Search',
+            user: 'https://randomuser.me/api/portraits/women/2.jpg',
+            description: 'A Pricing Page',
             link: '/share/nhC7sRElQh',
             width: 400,
             height: 300,
@@ -19,10 +16,7 @@ const DemoGallery = () => {
         },
         {
             src: 'https://image.memfree.me/memfree-generate-ui-wall-of-love.png',
-            user: {
-                avatar: 'https://randomuser.me/api/portraits/men/1.jpg',
-                name: 'John Smith',
-            },
+            user: 'https://randomuser.me/api/portraits/men/1.jpg',
             description: 'A wall of love',
             link: '/share/H13DXoooD5',
             width: 400,
@@ -31,11 +25,8 @@ const DemoGallery = () => {
         },
         {
             src: 'https://image.memfree.me/memfree-generate-ui-case-landing-page.png',
-            user: {
-                avatar: 'https://randomuser.me/api/portraits/women/3.jpg',
-                name: 'Alice Johnson',
-            },
-            description: 'A Landing Page for AI Search',
+            user: 'https://randomuser.me/api/portraits/women/3.jpg',
+            description: 'A Landing Page',
             link: '/share/422anMjcvm',
             width: 2318,
             height: 962,
@@ -43,10 +34,7 @@ const DemoGallery = () => {
         },
         {
             src: 'https://image.memfree.me/memfree-generate-ui-case-image-payment.png',
-            user: {
-                avatar: 'https://randomuser.me/api/portraits/men/4.jpg',
-                name: 'Bob Wilson',
-            },
+            user: 'https://randomuser.me/api/portraits/men/4.jpg',
             description: 'Payment Method UI Generated from Image',
             link: '/share/Ycjo4gsLgT',
             width: 1796,
@@ -55,10 +43,7 @@ const DemoGallery = () => {
         },
         {
             src: 'https://image.memfree.me/memfree-generate-ui-image-gallery.png',
-            user: {
-                avatar: 'https://randomuser.me/api/portraits/men/5.jpg',
-                name: 'Bob Wilson',
-            },
+            user: 'https://randomuser.me/api/portraits/men/5.jpg',
             description: 'A Image Gallery',
             link: '/share/ZDLk0pqYLl',
             width: 2250,
@@ -67,14 +52,29 @@ const DemoGallery = () => {
         },
         {
             src: 'https://image.memfree.me/memfree-generate-ui-case-clone-image.png',
-            user: {
-                avatar: 'https://randomuser.me/api/portraits/men/6.jpg',
-                name: 'Bob Wilson',
-            },
+            user: 'https://randomuser.me/api/portraits/men/6.jpg',
             description: 'Clone Any Static Page From Screenshot',
             link: '/share/suycUClVdH',
             width: 2260,
             height: 1076,
+            lazy: true,
+        },
+        {
+            src: 'https://image.memfree.me/memfree-generate-ui-stock-price.png',
+            user: 'https://randomuser.me/api/portraits/men/3.jpg',
+            description: `Real Stock Price With AI search`,
+            link: '/share/FI3zIsKdKk',
+            width: 2344,
+            height: 940,
+            lazy: true,
+        },
+        {
+            src: 'https://image.memfree.me/memfree-generate-ui-profile-image.png',
+            user: 'https://randomuser.me/api/portraits/women/1.jpg',
+            description: `Profile Card With AI search`,
+            link: '/share/FI3zIsKdKk',
+            width: 2344,
+            height: 940,
             lazy: true,
         },
     ];
@@ -100,8 +100,8 @@ const DemoGallery = () => {
                             </div>
                             <div className="p-4 flex items-center bg-white">
                                 <Avatar className="size-6 mr-2 flex-shrink-0">
-                                    <AvatarImage src={image.user.avatar} alt={image.user.name} />
-                                    <AvatarFallback>{image.user.name.charAt(0)}</AvatarFallback>
+                                    <AvatarImage src={image.user} />
+                                    <AvatarFallback>M</AvatarFallback>
                                 </Avatar>
                                 <p className="text-sm text-gray-600 line-clamp-2">{image.description}</p>
                             </div>
