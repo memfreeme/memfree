a318de01b7420c476559992428a087ff6fab5d15
ahaapple <work@ahaapple.com>
2024-10-19 14:41:08 +0800

    Unify all icon buttons style

--- frontend/components/search/action-buttons.tsx
+++ frontend/components/search/action-buttons.tsx
@@ -2,13 +2,14 @@ import React, { useMemo } from 'react';
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip';
 import { RefreshCcw, Share2, ThumbsDown, Trash2 } from 'lucide-react';
 import { Icons } from '@/components/shared/icons';
-import { Button, buttonVariants } from '@/components/ui/button';
+import { buttonVariants } from '@/components/ui/button';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
 import { SearchShareDialog } from '@/components/search/search-share-dialog';
 import { useTranslations } from 'next-intl';
 import useCopyToClipboard from '@/hooks/use-copy-clipboard';
 import { useSearchStore } from '@/lib/store/local-history';
+import IconButton from '@/components/layout/icon-button';
 
 const ActionButtons = ({ content, searchId, msgId, reload, searchType }) => {
     const [shareDialogOpen, setShareDialogOpen] = React.useState(false);
@@ -26,27 +27,13 @@ const ActionButtons = ({ content, searchId, msgId, reload, searchType }) => {
 
     const buttons = useMemo(
         () => (
-            <div className="flex space-x-4 mt-6">
-                <Tooltip>
-                    <TooltipTrigger asChild>
-                        <Button
-                            onClick={() => copyToClipboard(content)}
-                            variant="ghost"
-                            className="p-2 border-2 border-dashed rounded-full hover:bg-purple-300"
-                        >
-                            {hasCopied ? <Icons.check size={24} className="text-primary" /> : <Icons.copy size={24} className="text-primary" />}
-                        </Button>
-                    </TooltipTrigger>
-                    <TooltipContent className="font-bold">{t('Copy')}</TooltipContent>
-                </Tooltip>
-                <Tooltip>
-                    <TooltipTrigger asChild>
-                        <button onClick={handleReloadClick} className="p-2 border-2 border-dashed rounded-full text-primary hover:bg-purple-300">
-                            <RefreshCcw size={24} />
-                        </button>
-                    </TooltipTrigger>
-                    <TooltipContent className="font-bold">{t('Reload')}</TooltipContent>
-                </Tooltip>
+            <div className="flex space-x-4">
+                <IconButton onClick={() => copyToClipboard(content)} tooltipText={t('Copy')}>
+                    {hasCopied ? <Icons.check className="text-primary" /> : <Icons.copy className="text-primary" />}
+                </IconButton>
+                <IconButton onClick={handleReloadClick} tooltipText={t('Reload')}>
+                    <RefreshCcw className="text-primary" />
+                </IconButton>
                 <Tooltip>
                     <TooltipTrigger asChild>
                         <Link
@@ -57,35 +44,23 @@ const ActionButtons = ({ content, searchId, msgId, reload, searchType }) => {
                             className={cn(
                                 buttonVariants({
                                     variant: 'outline',
+                                    size: 'icon',
                                 }),
-                                'p-2 border-2 border-dashed rounded-full hover:bg-purple-300',
+                                '[&_svg]-h-3.5 size-7 rounded-[6px] [&_svg]:w-3.5',
                             )}
                         >
-                            <ThumbsDown size={24} className="text-primary" />
+                            <ThumbsDown className="text-primary" />
                         </Link>
                     </TooltipTrigger>
                     <TooltipContent className="font-bold">{t('Feedback')}</TooltipContent>
                 </Tooltip>
-                <Tooltip>
-                    <TooltipTrigger asChild>
-                        <button onClick={() => deleteMessage(msgId)} className="p-2 border-2 border-dashed rounded-full text-primary hover:bg-purple-300">
-                            <Trash2 size={24} />
-                        </button>
-                    </TooltipTrigger>
-                    <TooltipContent className="font-bold">Delete This Message</TooltipContent>
-                </Tooltip>
+                <IconButton onClick={() => deleteMessage(msgId)} tooltipText="Delete This Message">
+                    <Trash2 className="text-primary" />
+                </IconButton>
                 {isSearch && (
-                    <Tooltip>
-                        <TooltipTrigger asChild>
-                            <button
-                                onClick={() => setShareDialogOpen(true)}
-                                className="p-2 border-2 border-dashed rounded-full text-primary hover:bg-purple-300"
-                            >
-                                <Share2 size={24} />
-                            </button>
-                        </TooltipTrigger>
-                        <TooltipContent className="font-bold">{t('Share')}</TooltipContent>
-                    </Tooltip>
+                    <IconButton onClick={() => setShareDialogOpen(true)} tooltipText={t('Share')}>
+                        <Share2 className="text-primary" />
+                    </IconButton>
                 )}
                 {isSearch && (
                     <SearchShareDialog
