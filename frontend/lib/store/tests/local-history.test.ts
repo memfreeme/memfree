282771a6385c5df25964eb0abeaf2362c2852de8
ahaapple <work@ahaapple.com>
2024-10-29 19:38:58 +0800

    Fix typo and test wanring

--- frontend/lib/store/tests/local-history.test.ts
+++ frontend/lib/store/tests/local-history.test.ts
@@ -1,4 +1,4 @@
-import { act, renderHook } from '@testing-library/react-hooks';
+import { act, renderHook } from '@testing-library/react';
 import { useSearchStore } from '../local-history';
 import { Search, Message } from '@/lib/types';
 
