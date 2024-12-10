340943a961f49f7fc65b1cd29f5049bd10c22613
ahaapple <work@ahaapple.com>
2024-12-10 23:26:36 +0800

    Remove uploadthing

--- frontend/app/api/uploadthing/route.ts
+++
@@ -1,8 +0,0 @@
-import 'server-only';
-import { createRouteHandler } from 'uploadthing/next';
-
-import { ourFileRouter } from './core';
-
-export const { GET, POST } = createRouteHandler({
-    router: ourFileRouter,
-});
--- frontend/lib/uploadthing.ts
+++
@@ -1,5 +0,0 @@
-import { generateReactHelpers } from '@uploadthing/react';
-
-import type { OurFileRouter } from '@/app/api/uploadthing/core';
-
-export const { uploadFiles } = generateReactHelpers<OurFileRouter>();
