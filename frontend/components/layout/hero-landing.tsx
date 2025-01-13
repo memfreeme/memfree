4ade2d2794340bad155eff9bb8e5227c1a8e677e
ahaapple <work@ahaapple.com>
2025-01-13 14:08:11 +0800

    Update changelog

--- frontend/components/layout/hero-landing.tsx
+++ frontend/components/layout/hero-landing.tsx
@@ -1,41 +1,25 @@
-import { ImageGenerateUrl, PageGenUrl } from '@/config';
+import { PageGenUrl } from '@/config';
 import { getTranslations } from 'next-intl/server';
 import Link from 'next/link';
 import Image from 'next/image';
 
 export async function HeroLanding() {
     const t = await getTranslations('HomePage');
-    const isPageGen = Math.random() >= 0.5;
 
-    const PageGenElement = (
-        <Link href={PageGenUrl} target="_blank">
-            <div className="rounded-full px-6 py-2 flex items-center gap-2 hover:bg-primary/10 transition-all duration-300">
-                <Image src="/logo.png" alt="PageGen" width={30} height={30} className="size-6" />
-                <span>PageGen - AI Page Generator</span>
-                <span className="ml-2 bg-gradient-to-r from-blue-500 to-purple-500 text-white px-4 py-1 rounded-full text-sm font-medium hover:shadow-lg transition-all">
-                    Try Free Now
-                </span>
-            </div>
-        </Link>
-    );
-
-    const AhaToolElement = (
-        <Link href={ImageGenerateUrl} target="_blank">
-            <div className="rounded-full px-6 py-2 flex items-center gap-2 hover:bg-primary/10 transition-all duration-300">
-                <Image src="/logo.png" alt="AI Image Generator" width={30} height={30} className="size-6" />
-                <span>New: AI Image Generator</span>
-                <span className="ml-2 bg-gradient-to-r from-blue-500 to-purple-500 text-white px-4 py-1 rounded-full text-sm font-medium hover:shadow-lg transition-all">
-                    Try Free Now
-                </span>
-            </div>
-        </Link>
-    );
     return (
         <section className="py-10">
             <div className="flex flex-col items-center text-center space-y-4">
                 <h1 className="text-balance text-4xl md:text-5xl font-bold">{t('hero')}</h1>
                 <h2 className="hidden ">MemFree - Hybrid AI Search</h2>
-                {isPageGen ? <>{PageGenElement}</> : <>{AhaToolElement}</>}
+                <Link href={PageGenUrl} target="_blank">
+                    <div className="rounded-full px-6 py-2 flex items-center gap-2 hover:bg-primary/10 transition-all duration-300">
+                        <Image src="/logo.png" alt="PageGen" width={30} height={30} className="size-6" />
+                        <span>PageGen - AI Page Generator</span>
+                        <span className="ml-2 bg-gradient-to-r from-blue-500 to-purple-500 text-white px-4 py-1 rounded-full text-sm font-medium hover:shadow-lg transition-all">
+                            Try Free Now
+                        </span>
+                    </div>
+                </Link>
             </div>
         </section>
     );
