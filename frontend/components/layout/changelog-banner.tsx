5e6775f009676cd716b6f250d01926595aeeeeec
ahaapple <work@ahaapple.com>
2025-01-21 19:41:45 +0800

    Update changelog for DeepSeek R1

--- frontend/components/layout/changelog-banner.tsx
+++ frontend/components/layout/changelog-banner.tsx
@@ -5,7 +5,7 @@ import { X } from 'lucide-react';
 
 export default function ChangelogBanner() {
     const [isVisible, setIsVisible] = useState(true);
-    const bannerKey = 'changelog-search-generator';
+    const bannerKey = 'changelog-deepseek-r1';
 
     useEffect(() => {
         const isBannerClosed = localStorage.getItem(bannerKey);
@@ -25,7 +25,7 @@ export default function ChangelogBanner() {
         <div className="hidden md:flex relative text-sm isolate  items-center gap-x-6 overflow-hidden bg-primary text-violet-100 px-6 py-2.5 sm:px-3.5">
             <div className="flex w-full flex-wrap items-center justify-center gap-x-4 gap-y-2 font-bold">
                 <p>
-                    <span>MemFree Supports AI-Powered Search History Now 🎉 </span>
+                    <span>MemFree Supports DeepSeek R1 Reasoning Model Now 🎉 </span>
                 </p>
                 <Link href="/changelog" target="_black" className="underline">
                     Learn More
