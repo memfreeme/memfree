6f4718c158c4bc7556af8000e1f6c7077cfb0638
ahaapple <work@ahaapple.com>
2025-02-06 20:05:48 +0800

    Update changelog

--- frontend/components/layout/changelog-banner.tsx
+++ frontend/components/layout/changelog-banner.tsx
@@ -5,7 +5,7 @@ import { X } from 'lucide-react';
 
 export default function ChangelogBanner() {
     const [isVisible, setIsVisible] = useState(true);
-    const bannerKey = 'changelog-deepseek-r1';
+    const bannerKey = 'changelog-openai-o1';
 
     useEffect(() => {
         const isBannerClosed = localStorage.getItem(bannerKey);
@@ -25,7 +25,7 @@ export default function ChangelogBanner() {
         <div className="hidden md:flex relative text-sm isolate  items-center gap-x-6 overflow-hidden bg-primary text-violet-100 px-6 py-2.5 sm:px-3.5">
             <div className="flex w-full flex-wrap items-center justify-center gap-x-4 gap-y-2 font-bold">
                 <p>
-                    <span>MemFree Supports DeepSeek R1 Reasoning Model Now 🎉 </span>
+                    <span>MemFree Supports OpenAI O3-Mini and OpenAI O1 Models Now 🎉 </span>
                 </p>
                 <Link href="/changelog" target="_black" className="underline">
                     Learn More
@@ -36,7 +36,7 @@ export default function ChangelogBanner() {
                 className="absolute right-4 top-1/2 -translate-y-1/2 p-1 hover:opacity-80 transition-opacity"
                 aria-label="Close banner"
             >
-                <X className="h-4 w-4" />
+                <X className="size-4" />
             </button>
         </div>
     );
